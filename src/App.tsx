33de8189e799d013e4e14d0dab0daa909615bb06
justinWGU <Justin_ortiz123@yahoo.com>
2025-12-15 23:15:09 -0800

    Updates styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,25 @@ function App() {
     <div className='bg-black'>
 
       {/* Hero Section */}
-      <div className='flex flex-col items-center justify-center min-h-screen text-white'>
-        <h1 className='text-7xl font-semibold tracking-tight'>Justin Ortiz</h1>
-        <p className='text-sm mt-6 tracking-[0.3em] uppercase'>Fullstack Software Engineer</p>
-        <nav className='flex gap-8 mt-6 text-gray-300 hover:text-white hover:cursor-pointer transition'>
+      <section id='intro' className='flex flex-col items-center justify-center h-screen text-white'>
+        <h1 className='text-5xl md:text-7xl font-semibold tracking-tight'>Justin Ortiz</h1>
+        <p className='text-sm mt-4 tracking-[0.3em] uppercase'>Fullstack Software Engineer</p>
+        <nav className='flex gap-8 mt-10 text-gray-300 hover:text-white hover:cursor-pointer transition'>
           <a href='#projects'>Projects</a>
           <a href='#about'>About</a>
           <a href='#contact'>Contact</a>
         </nav>
-      </div>
+      </section>
 
       {/* Projects */}
-      <div id='projects' className='min-h-screen px-30 gap-4 flex flex-col justify-center'>
+      <section id='projects' className='min-h-screen max-w-3xl mx-auto px-6 py-24 gap-4 flex flex-col'>
         <h2 className='text-sm uppercase tracking-widest text-gray-400'>Projects</h2>
         
           {/* Proj 1 */}
-          <div className='flex flex-col gap-4'>
-
-          <div className='flex flex-col p-6 border border-white/10 bg-white/5 rounded-xl'>
+          <div className='flex flex-col gap-2 p-6 border border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
               <h4 className='text-lg text-white'>Call of Duty Scoreboard</h4>
-              <p className='mt-4 text-gray-200'>Developed and deployed a full stack web app to display real-time Call of Duty scores. Used OpenCV to extract scores from live stream frames and an LLM API to convert data to structured output for a Django REST API.</p>
-              <div className='flex gap-2 mt-4 text-gray-400'>
+              <p className='text-gray-200'>Developed and deployed a full stack web app to display real-time Call of Duty scores. Used OpenCV to extract scores from live stream frames and an LLM API to convert data to structured output for a Django REST API.</p>
+              <div className='flex gap-2 text-gray-400'>
                 <p>React •</p>                
                 <p>Django •</p>
                 <p>Google Genai •</p>
@@ -36,10 +34,10 @@ function App() {
           </div>
           
           {/* Proj 2 */}
-          <div className='flex flex-col p-6 border border-white/10 bg-white/5 rounded-xl'>
+          <div className='flex flex-col p-6 gap-2 border border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
               <h4 className='text-lg text-white'>Intelligent RC Robot</h4>
-              <p className='mt-4 text-gray-200'>Built a web app for manual and autonomous robot control. Backend runs on a Raspberry Pi using Django, OpenCV, and a local LLM to generate movement commands sent via UART to an STM32 microcontroller. </p> 
-              <div className='flex gap-2 mt-4 text-gray-400'>
+              <p className='text-gray-200'>Built a web app for manual and autonomous robot control. Backend runs on a Raspberry Pi using Django, OpenCV, and a local LLM to generate movement commands sent via UART to an STM32 microcontroller. </p> 
+              <div className='flex gap-2 text-gray-400'>
                 <p>React •</p>                
                 <p>Django •</p>
                 <p>C •</p>
@@ -49,34 +47,33 @@ function App() {
           </div>
 
           {/* Proj 3 */}
-          <div className='flex flex-col p-6 border border-white/10 bg-white/5 rounded-xl'>
+          <div className='flex flex-col p-6 border gap-2 border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
               <h4 className='text-lg text-white'>Hotel Booking App Enhancement and Dockerization</h4>
-              <p className='mt-4 text-gray-200'>Enhanced an app with multi-threading, timezone messaging, and real-time currency exchange.</p>
-              <div className='flex gap-2 mt-4 text-gray-400'>
+              <p className='text-gray-200'>Enhanced an app with multi-threading, timezone messaging, and real-time currency exchange.</p>
+              <div className='flex gap-2 text-gray-400'>
                 <p>Spring Boot •</p>                
                 <p>Angular •</p>           
                 <p>Docker</p>
               </div>
           </div>      
-        </div>
-      </div>
+      </section>
 
       {/* About */}
-      <div id='about' className='flex flex-col justify-center px-30 mt-6 min-h-[40vh]'>
+      <section id='about' className='flex flex-col gap-4 max-w-3xl mx-auto px-12 py-24'>
         <h3 className='text-sm text-gray-400 uppercase tracking-widest'>About</h3>
-        <p className='mt-6 max-w-lg text-gray-300'>I create intelligent full-stack web apps that integrate AI like LLMs and computer vision. I also bring embedded systems expertise, connecting these advanced technologies with hardware to create innovative solutions.</p>
-      </div>
+        <p className='max-w-lg text-gray-300'>I create intelligent full-stack web apps that integrate AI like LLMs and computer vision. I also bring embedded systems expertise, connecting these advanced technologies with hardware to create innovative solutions.</p>
+      </section>
 
       {/* Contact */}
-      <div id='contact' className='flex flex-col justify-center mt-6 px-30 min-h-[40vh]'>
+      <section id='contact' className='flex flex-col gap-4 mx-auto max-w-3xl px-12 py-24'>
         <h3 className='text-sm text-gray-400 uppercase tracking-widest'>Contact</h3>
-        <p className='mt-6 text-gray-300'>Interested in working together or just want to say hello?</p>
-        <div className='flex gap-8 mt-6 text-gray-300'>
+        <p className='text-gray-300'>Interested in working together or just want to say hello?</p>
+        <div className='flex gap-8 text-gray-300'>
           <a className='transition hover:text-white' href='mailto:justino9921@gmail.com'>Email</a>
           <a className='transition hover:text-white' href='https://www.linkedin.com/in/justinortiz21/'>LinkedIn</a>
           <a className='transition hover:text-white' href='https://github.com/justinWGU'>GitHub</a>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
