45d343ef91fee833a7d3c82eca8a47edbe6e66e9
justinWGU <Justin_ortiz123@yahoo.com>
2025-12-17 13:53:01 -0800

    Moves contact links to the top, adds resume link, changes styling.

--- src/App.tsx
+++ src/App.tsx
@@ -6,72 +6,87 @@ function App() {
     <div className='bg-black'>
 
       {/* Hero Section */}
-      <section id='intro' className='flex flex-col items-center justify-center h-screen text-white'>
+      <section id='intro' className='py-24 px-6 flex flex-col max-w-5xl mx-auto text-white'>
         <h1 className='text-5xl md:text-7xl font-semibold tracking-tight'>Justin Ortiz</h1>
-        <p className='text-xs md:text-sm mt-4 tracking-[0.3em] uppercase text-white/70'>Fullstack Software Engineer</p>
-        <nav className='flex gap-8 mt-10 text-sm text-white/60 hover:cursor-pointer'>
-          <a className='transition hover:text-white' href='#projects'>Projects</a>
-          <a className='transition hover:text-white' href='#about'>About</a>
-          <a className='transition hover:text-white' href='#contact'>Contact</a>
-        </nav>
+        <p className='leading-relaxed max-w-3xl mt-4 text-white/70'>
+          Entry-level Software Engineer focused on full-stack web development, AI integration, and embedded systems.        </p>
+        <div className='flex gap-8 mt-10 text-sm text-white/60 hover:cursor-pointer'>
+          <a className='transition hover:text-white underline' target='_blank' href='https://www.linkedin.com/in/justinortiz21/'>LinkedIn</a>
+          <a className='transition hover:text-white underline' target='_blank' href='https://github.com/justinWGU'>GitHub</a>
+          <a className='transition hover:text-white underline' target='_blank' href='/public/JustinOrtizResume2025.pdf'>Resume (PDF)</a>
+        </div>
       </section>
 
       {/* Projects */}
-      <section id='projects' className='min-h-screen max-w-3xl mx-auto px-6 py-24 gap-4 flex flex-col'>
+      <section id='projects' className='max-w-5xl mx-auto px-6 py-24 gap-4 flex flex-col'>
         <h2 className='text-sm mb-12 uppercase tracking-widest text-white/60'>Projects</h2>
         
           {/* Proj 1 */}
           <div className='flex flex-col gap-2 p-6 border border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
-              <h4 className='text-lg text-white font-medium'>Call of Duty Scoreboard</h4>
-              <p className='leading-relaxed text-white/65'>Developed and deployed a full stack web app to display real-time Call of Duty scores. Used OpenCV to extract scores from live stream frames and an LLM API to convert data to structured output for a Django REST API.</p>
-              <div className='flex gap-2 text-white/50'>
-                <p>React •</p>                
-                <p>Django •</p>
-                <p>Google Genai •</p>
-                <p>OpenCV</p>
-                <p></p>
+              <h4 className='text-lg text-white font-medium'>Live Video Game Scoreboard</h4>
+              <p className='leading-relaxed text-white/60'>
+                A live scoreboard that quickly displays Call of Duty competitive match scores without the need to sign in or join a live stream.
+              </p>
+              <div className='flex gap-2 text-sm text-white/50'>
+                <p>React •Django • Google Genai • OpenCV</p>
               </div>
+              <ul className='text-white/65 mt-2 leading-relaxed space-y-2 list-disc list-inside'>
+                <li>Leveraged computer vision to capture a snapshot from a live stream</li>
+                <li>Utilized an LLM API to extract the scores from the snapshot</li>
+                <li>Developed a RESTful API to expose the captured scores for consumption</li>
+              </ul>
           </div>
           
           {/* Proj 2 */}
           <div className='flex flex-col p-6 gap-2 border border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
               <h4 className='text-lg text-white'>Intelligent RC Robot</h4>
-              <p className='leading-relaxed text-white/65'>Built a web app for manual and autonomous robot control. Backend runs on a Raspberry Pi using Django, OpenCV, and a local LLM to generate movement commands sent via UART to an STM32 microcontroller. </p> 
-              <div className='flex gap-2 text-white/50'>
-                <p>React •</p>                
-                <p>Django •</p>
-                <p>C •</p>
-                <p>OpenCV</p>
+              <p className='leading-relaxed text-white/60'>
+                An intelligent robot operated manually or autonomously, serving to demonstrate the capabilities of interfacing advanced AI technologies with real hardware control.
+              </p> 
+              <div className='flex gap-2 text-sm text-white/50'>
+                <p>React • Django • Llama3 • OpenCV • C • Raspberry Pi • STM32</p>
               </div>
+              <ul className='text-white/65 mt-2 leading-relaxed space-y-2 list-disc list-inside'>
+                <li>Leveraged computer vision to access camera connected to Raspberry Pi</li>
+                <li>Developed RESTful API to expose video data for a visual interface</li>
+                <li>Built an inteface to display an interactive POV of the robot and control its peripherals</li>
+                <li>Established UART-based communication between an STM32 microcontroller and a Raspberry Pi</li>
+              </ul>
           </div>
 
           {/* Proj 3 */}
           <div className='flex flex-col p-6 border gap-2 border-white/10 bg-white/5 rounded-xl transition hover:bg-white/10'>
-              <h4 className='text-lg text-white'>Hotel Booking App Enhancement and Dockerization</h4>
-              <p className='leading-relaxed text-white/65'>Enhanced an app with multi-threading, timezone messaging, and real-time currency exchange.</p>
-              <div className='flex gap-2 text-white/50'>
-                <p>Spring Boot •</p>                
-                <p>Angular •</p>           
-                <p>Docker</p>
+              <h4 className='text-lg text-white'>Developer Portfolio Website</h4>
+              <p className='leading-relaxed text-white/60'>
+                My comprehensive personal website serving to showcase my range of skills in fullstack web development, AI integration, and embedded systems.
+              </p>
+              <div className='flex gap-2 text-sm text-white/50'>
+                <p>React • TailwindCSS • Netlify</p>
               </div>
+              <ul className='text-white/65 mt-2 leading-relaxed space-y-2 list-disc list-inside'>
+                <li>Applied modern tools to design and develop a polished frontend</li>
+                <li>Utilized Netlify to publicy display the frontend from a custom URL</li>
+              </ul>
           </div>      
       </section>
 
       {/* About */}
-      <section id='about' className='flex flex-col gap-4 max-w-3xl mx-auto px-12 py-24'>
-        <h3 className='text-sm text-white/60 uppercase tracking-widest'>About</h3>
-        <p className='max-w-lg text-white/70'>I create intelligent full-stack web apps that integrate AI like LLMs and computer vision. I also bring embedded systems expertise, connecting these advanced technologies with hardware to create innovative solutions.</p>
+      <section id='about' className='flex flex-col gap-4 max-w-5xl mx-auto px-12 py-24'>
+        <h3 className='max-w-3xl text-sm text-white/60 uppercase tracking-widest'>About</h3>
+        <p className='text-white/70'>
+          I am a CS graduate with a strong interest in web development and embedded systems. I have a keen interest in combining advanced technologies, like LLMs, and MCUs to invent physical solutions to real world problems.
+        </p>
       </section>
 
-      {/* Contact */}
-      <section id='contact' className='flex flex-col gap-4 mx-auto max-w-3xl px-12 py-24'>
-        <h3 className='text-sm text-white/60 uppercase tracking-widest'>Contact</h3>
-        <p className='text-white/70'>Interested in working together or just want to say hello?</p>
-        <div className='flex gap-8 text-white/60'>
-          <a className='transition hover:text-white' href='mailto:justino9921@gmail.com'>Email</a>
-          <a className='transition hover:text-white' href='https://www.linkedin.com/in/justinortiz21/'>LinkedIn</a>
-          <a className='transition hover:text-white' href='https://github.com/justinWGU'>GitHub</a>
-        </div>
+      {/* Back to Top */}
+      <section id='back-to-top' className='py-24 px-12 max-w-5xl mx-auto'>
+        <a href='#intro' className='p-5 m-2 border text-white/50 border-white/30 hover:bg-white hover:text-black'>Back to top</a>
+        <a href='mailto:justino9921@gmail.com' className='p-5 m-2 border text-white/50 border-white/30 transition hover:bg-white hover:text-black'>Email</a>
+      </section>
+
+      {/* Footer */}
+      <section id='footer' className='text-white/40 max-w-xs text-xs px-12 pb-5 pt-24 mx-auto'>
+        © {new Date().getFullYear()} Justin Ortiz
       </section>
     </div>
   )
